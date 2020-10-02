1a55504d3936605613891dd0f66943b2901635fc
Matti Mäki-Kihniä <matti.makikihnigmail.com>
2020-10-02 19:18:05 +0300

    master Spotless

--- app/src/main/java/com/github/harmittaa/touchobserver/application/TouchRecognizerApplication.kt
+++ app/src/main/java/com/github/harmittaa/touchobserver/application/TouchRecognizerApplication.kt
@@ -1,13 +1,13 @@
 package com.github.harmittaa.touchobserver.application
 
 import android.app.Application
+import com.github.harmittaa.touchobserver.BuildConfig
 import com.github.harmittaa.touchobserver.di.authModule
 import com.github.harmittaa.touchobserver.di.dataStoreModule
 import com.github.harmittaa.touchobserver.di.firebaseModule
 import com.github.harmittaa.touchobserver.di.logicModule
 import com.github.harmittaa.touchobserver.di.repositoryModule
 import com.github.harmittaa.touchobserver.di.viewModelModule
-import com.github.harmittaa.touchobserver.BuildConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
